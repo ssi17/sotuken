589b61f17adca2b4fec927ec87f6d43f91b24e34
Yu <yu-ashitomi-19e@stu.kbc.ac.jp>
2022-06-21 10:33:55 +0900

    add location permission

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -12,22 +16,39 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1234
+    }
+
     private lateinit var navController: NavController
     var bgm: MediaPlayer = MediaPlayer()
+    var permissionFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ナビゲーションの設定
+        // 下部ナビゲーション
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        // フラグメント間の遷移
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-//        setupActionBarWithNavController(navController)
         setupWithNavController(bottomNavigation, navController)
 
+        // 位置情報の利用許可
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            permissionFlag = true
+        } else {
+            // 許可を求めるダイアログを表示
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
+
+        // BGMの設定
         bgm = MediaPlayer.create(this, R.raw.bgm)
-        bgm.isLooping = true
-        bgm.start()
+        bgm.isLooping = true    // ループ再生をON
+        bgm.start()             // BGMを再生
     }
 
     fun setButton(startButton: ImageView, flag: Boolean) {
@@ -37,5 +58,18 @@ class MainActivity : AppCompatActivity() {
             } else {
                 R.drawable.start_button
             })
+    }
+
+    // 位置情報の利用許可の結果を確認
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == PERMISSION_REQUEST_CODE) {
+            permissionFlag = grantResults[0] == PackageManager.PERMISSION_GRANTED
+        }
     }
 }
