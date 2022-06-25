58472b683b9c01432a9efbd9d06a3979c6e64f2d
Yu <yu-ashitomi-19e@stu.kbc.ac.jp>
2022-06-26 01:58:31 +0900

    open url

--- app/src/main/java/com/example/myapplication/ui/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication/ui/RecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -19,16 +21,26 @@ class RecyclerAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // サムネイル画像を設定
         holder.images.setImageResource(images?.get(position) as Int)
+
         // お気に入り登録ボタンの設定
         if(favoriteFlag?.get(position) == true) {
             holder.favorites.setImageResource(R.drawable.favorite_button)
         } else {
             holder.favorites.setImageResource(R.drawable.not_favorite_button)
         }
+
         // タイトルを設定
         holder.titles.setText(titles?.get(position) as Int)
+
         // 説明を設定
         holder.describes.setText(describes?.get(position) as Int)
+
+        val context = holder.item.context
+        // ページへ
+        holder.pages.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://oki-park.jp/shurijo/about"))
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/myapplication/ui/ViewHolder.kt
+++ app/src/main/java/com/example/myapplication/ui/ViewHolder.kt
@@ -8,9 +8,10 @@ import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 
-class ViewHolder(item: View): RecyclerView.ViewHolder(item) {
+class ViewHolder(val item: View): RecyclerView.ViewHolder(item) {
     val images: ImageView = item.findViewById(R.id.item_image)
     val favorites: ImageButton = item.findViewById(R.id.favorite_button)
     val titles: TextView = item.findViewById(R.id.item_title)
     val describes: TextView = item.findViewById(R.id.item_describe)
+    val pages: TextView = item.findViewById(R.id.item_page)
 }
